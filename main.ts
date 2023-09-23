33316ef9af73126f167635f549f0b76cbe924d49
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-09-23 19:20:27 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
-//@ts-ignore
 import BigBrainCompiler from "./bigbrain/scripts/bigbrain.ts";
-//@ts-ignore
 import BrainfuckCompiler from "./compiler.ts";
 
-//@ts-ignore
 BigBrainCompiler(Deno.readTextFileSync(new URL(Deno.args[0], import.meta.url))).then((output: string) => {
     Deno.writeTextFileSync("./" + Deno.args[0].replace(".bb", "") + ".bf", output)
     console.log(BrainfuckCompiler(output));
